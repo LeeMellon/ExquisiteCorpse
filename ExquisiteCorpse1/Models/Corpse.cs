ca2f910ccb76d8897c42b5e2e9692cb7d9144bfd
Ian Goodrich <iangoodrich@hotmail.com>
2018-05-16 22:09:43 -0700

    Update Corpse Model. Problem Updating TestDbContext

--- ExquisiteCorpse1/Models/Corpse.cs
+++ ExquisiteCorpse1/Models/Corpse.cs
@@ -18,33 +18,72 @@ namespace ExquisiteCorpse1.Models
         public List<ApplicationUser> Players { get; set; }
         public int Rounds { get; set; }
         public int CurrentRound { get; set; }
-        public int CurrentPlayerInedx { get; set; }
+        public int CurrentPlayerIndex { get; set; }
         public List<UserCorpse> UserCorpses { get; set; }
+        public string Status { get; set; } //make Enum?
 
 
        public Corpse(int currentRound = 0, int currrentPlayerIndex =0) { }
 
-       
+        public string GetLastStub()
+        {
+
+            return Sections[Sections.Count - 1].Stub;
+        }
+
+       public string GetLastPlayerName()
+        {
+            return Players[CurrentPlayerIndex - 1].ProfileName;
+        }
+
+        public string GetNextPlayerNumber()
+        {
+            return Players[CurrentPlayerIndex].PhoneNumber;
+        }
+
+        public void SendInvite()
+        {
+            var invitePlayers = Players.ToList().Skip(1);
+            foreach(ApplicationUser player in invitePlayers)
+            {
+                var body = Players[0].ProfileName + "has invited you to make a corpse";
+                var to = "+1" + player.PhoneNumber;
+                new TextMessage { To = to, Body = body }.Send();
+            }
+        }
+
+       public void NextTurn()
+        {
+
+            var body = "It's your turn. " + GetLastPlayerName() + "has given you this to work with: " + GetLastStub() + "Go to INSERTROUTEHERE to continue the story.";
+            new TextMessage { To = "+1" + GetNextPlayerNumber() , Body = body, }.Send();
+            
+        }
 
 
        public void EndGame()
         {
-            //var playerNumbers = 
-            //new TextMessage completionMessage = new TextMessage { }
+            var body = "It's Alive! Go to https://www.ThisExquisiteCorpse.com/Corpse/" + CorpseId + " to read what you've created";
+            foreach (ApplicationUser p in Players)
+            {
+                 new TextMessage { To = "+1" + p.PhoneNumber, Body = body, }.Send();
+            }
         }
 
-       public void Increment()
+        public void Increment()
         {
-            if(CurrentPlayerInedx < Players.Count)
+            if(CurrentPlayerIndex < Players.Count)
             {
-                CurrentPlayerInedx += 1;
+                CurrentPlayerIndex += 1;
+                NextTurn();
             }
-            else if((CurrentPlayerInedx == Players.Count)&&(CurrentRound < Rounds))
+            else if((CurrentPlayerIndex == Players.Count)&&(CurrentRound < Rounds))
             {
                 CurrentRound += 1;
-                CurrentPlayerInedx = 0;
+                CurrentPlayerIndex = 0;
+                NextTurn();
             }
-            else if ((CurrentPlayerInedx == Players.Count)&&(CurrentRound == Rounds))
+            else if ((CurrentPlayerIndex == Players.Count)&&(CurrentRound == Rounds))
             {
                 EndGame();
             }
