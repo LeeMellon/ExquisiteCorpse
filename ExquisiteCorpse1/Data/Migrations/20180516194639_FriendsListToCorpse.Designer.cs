408f9aeba564b2408856f8452542c904dcd64b77
Ian Goodrich <iangoodrich@hotmail.com>
2018-05-16 18:43:38 -0700

    add migrations

--- ExquisiteCorpse1/Data/Migrations/20180516194639_FriendsListToCorpse.Designer.cs
+++ ExquisiteCorpse1/Data/Migrations/20180516194639_FriendsListToCorpse.Designer.cs
@@ -8,9 +8,10 @@ using ExquisiteCorpse1.Models;
 namespace ExquisiteCorpse1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180516194639_FriendsListToCorpse")]
+    partial class FriendsListToCorpse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");
