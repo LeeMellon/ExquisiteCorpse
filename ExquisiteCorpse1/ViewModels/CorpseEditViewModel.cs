3eb79d4c889c9ac270b3d5103d55337d2485b6ec
Ian Goodrich <iangoodrich@hotmail.com>
2018-05-16 18:40:44 -0700

    add CorpseEditViewMdel and ass. methods

---
+++ ExquisiteCorpse1/ViewModels/CorpseEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ExquisiteCorpse1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExquisiteCorpse1.ViewModels
+{
+    public class CorpseEditViewModel
+    {
+        public List<string> Stubs { get; set; }
+        public Corpse Corpse { get; set; }
+        public List<string> ProfileNames { get; set; }
+    }
+}
