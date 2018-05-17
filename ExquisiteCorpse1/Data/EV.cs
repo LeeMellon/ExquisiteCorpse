860eeb25e5d0d9ce9d67a2b365920c4cae6b44a4
Ian Goodrich <iangoodrich@hotmail.com>
2018-05-16 18:40:04 -0700

    aded Text message class and send method

---
+++ ExquisiteCorpse1/Data/EV.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExquisiteCorpse1.Data
+{
+    public class EV
+    {
+        public static string AccountSid = "AC570c52bc6988afc6053be6e0b527f4a5";
+        public static string AuthToken = "9b502b6d1863a835f0682998fba950e5";
+    }
+}
---
+++ ExquisiteCorpse1/Models/TextMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ExquisiteCorpse1.Data;
+
+namespace ExquisiteCorpse1.Models
+{
+    public class TextMessage
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+
+        public TextMessage( )
+        {
+            From = "+15416251981";
+        }
+
+        public void Send()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/" + EV.AccountSid + "/Messages", Method.POST);
+            request.AddParameter("To", To);
+            request.AddParameter("From", From);
+            request.AddParameter("Body", Body);
+            client.Authenticator = new HttpBasicAuthenticator(EV.AccountSid, EV.AuthToken);
+            client.ExecuteAsync(request, response =>
+            {
+                Console.WriteLine(response.Content);
+            });
+        }
+
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+
+}
+
