306352620c66c4bfbe954087b0e6a82b880c41b9
Ian Goodrich <iangoodrich@hotmail.com>
2018-05-31 02:14:51 -0700

    getting closer

--- ExquisiteCorpse1/Controllers/ApplicationUserController.cs
+++ ExquisiteCorpse1/Controllers/ApplicationUserController.cs
@@ -48,65 +48,67 @@ namespace ExquisiteCorpse1.Controllers
             var thisList = _db.UserCorpses.Where(uc => uc.UserId == thisUser.Id).Select(uc => uc.CorpseId).ToList();
             foreach(int id in thisList)
             {
-            var testEms = _db.Corpses.Include(c => c.Players).FirstOrDefault(c => c.CorpseId == id);
+                var testEms = _db.Corpses.Include(c => c.Players).FirstOrDefault(c => c.CorpseId == id);
+                corpseList.Add(new CorpseDisplayAllViewModel { Corpse = testEms, Players = testEms.Players });
+
 
             }
-            //        if (viewOption == "All")
-            //        {
-            //            foreach (Corpse c in thisList)
-            //            {
-            //                corpseList.Add(new CorpseDisplayAllViewModel { Corpse = c });
-            //            }
+            if (viewOption == "All")
+            {
+                //foreach (Corpse c in thisList)
+                //{
+                //    corpseList.Add(new CorpseDisplayAllViewModel { Corpse = c });
+                //}
 
-            return View(corpseList);
+                return View(corpseList);
 
-            //        }
-            //        else
-            //        {
-            //            List<Corpse> thisModel = new List<Corpse> { };
-            //            var testr = viewOption;
-            //            var listetst = thisList;
-            //            foreach (Corpse c in thisList)
-            //            {
-            //                var test = viewOption;
+            }
+            else
+            {
+                //    List<Corpse> thisModel = new List<Corpse> { };
+                //    var testr = viewOption;
+                //    var listetst = thisList;
+                //    foreach (Corpse c in thisList)
+                //    {
+                //        var test = viewOption;
 
-            //                if (viewOption == "Turn")
-            //                {
-            //                    if (c.GetCurrentPlayerId() == thisUser.Id)
-            //                    {
-            //                        thisModel.Add(c);
-            //                    }
+                //        if (viewOption == "Turn")
+                //        {
+                //            if (c.GetCurrentPlayerId() == thisUser.Id)
+                //            {
+                //                thisModel.Add(c);
+                //            }
 
-            //                }
-            //                else if (viewOption == "Awaiting")
-            //                {
-            //                    if (c.Status == "Awaiting")
-            //                    {
-            //                        thisModel.Add(c);
-            //                    }
+                //        }
+                //        else if (viewOption == "Awaiting")
+                //        {
+                //            if (c.Status == "Awaiting")
+                //            {
+                //                thisModel.Add(c);
+                //            }
 
-            //                }
-            //                else if (viewOption == "Waiting")
-            //                {
-            //                    if ((c.Status == "Active") && (c.GetCurrentPlayerId() != thisUser.Id))
-            //                    {
-            //                        thisModel.Add(c);
-            //                    }
+                //        }
+                //        else if (viewOption == "Waiting")
+                //        {
+                //            if ((c.Status == "Active") && (c.GetCurrentPlayerId() != thisUser.Id))
+                //            {
+                //                thisModel.Add(c);
+                //            }
 
-            //                }
-            //                else if (viewOption == "Complete")
-            //                {
-            //                    if (c.Status == "Complete")
-            //                    {
-            //                        thisModel.Add(c);
-            //                    }
+                //        }
+                //        else if (viewOption == "Complete")
+                //        {
+                //            if (c.Status == "Complete")
+                //            {
+                //                thisModel.Add(c);
+                //            }
 
-            //                }
-            //            }
+                //        }
+                //    }
 
-            //            return View(thisModel);
+                return View(corpseList);
 
-            //        }
+            }
         }
     }
     }
